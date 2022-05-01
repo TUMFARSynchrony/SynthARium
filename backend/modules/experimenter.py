c919b953f7d224d09c7b518903a6251f5009b0a5
Alexander Liebald <alexander.liebald@gmail.com>
2022-05-01 13:24:44 +0200

    Experimenter: add documentation

--- backend/modules/experimenter.py
+++ backend/modules/experimenter.py
@@ -1,4 +1,4 @@
-"""TODO document"""
+"""This module provides the Experimenter class."""
 
 from __future__ import annotations
 from aiortc import RTCSessionDescription
@@ -17,15 +17,29 @@ import modules.user as _user
 
 
 class Experimenter(_user.User):
-    """TODO document"""
+    """Experimenter is a type of modules.user.User with experimenter rights.
+
+    Has access to a different set of API endpoints than other Users.  API endpoints for
+    experimenters are defined here.
+    """
 
     _experiment: _experiment.Experiment
     _hub: _hub.Hub
 
     def __init__(self, id: str, hub: _hub.Hub):
-        """TODO document"""
+        """Instantiate new Experimenter instance.
+
+        Parameters
+        ----------
+        id : str
+            Unique identifier for this Experimenter.
+        hub : modules.hub.Hub
+            Hub this Experimenter is part of.  Used for api calls.
+        """
         super().__init__(id)
         self._hub = hub
+
+        # Add API endpoints
         self.on("GET_SESSION_LIST", self._handle_get_session_list)
         self.on("SAVE_SESSION", self._handle_save_session)
         self.on("DELETE_SESSION", self._handle_delete_session)
@@ -36,12 +50,48 @@ class Experimenter(_user.User):
         self.on("ADD_NOTE", self._handle_add_note)
 
     def _handle_get_session_list(self, _):
-        """TODO document"""
+        """Handle requests with type `GET_SESSION_LIST`.
+
+        Loads all known sessions from session manager.  Responds with type:
+        `SESSION_LIST`.
+
+        Parameters
+        ----------
+        _ : any
+            Message data.  Ignored / not required.
+
+        Returns
+        -------
+        custom_types.message.MessageDict
+            MessageDict with type: `SESSION_LIST` and data: list of
+            custom_types.session.SessionDict.
+        """
         sessions = self._hub.session_manager.get_session_dict_list()
         return MessageDict(type="SESSION_LIST", data=sessions)
 
     def _handle_save_session(self, data):
-        """TODO document"""
+        """Handle requests with type `SAVE_SESSION`.
+
+        Checks if received data is a valid SessionDict.  Try to update existing session,
+        if `id` exists in data, otherwise try to create new session.
+
+        Parameters
+        ----------
+        data : any
+            Message data.  Checks if data is valid custom_types.session.SessionDict.
+
+        Returns
+        -------
+        custom_types.message.MessageDict
+            MessageDict with type: `SUCCESS`, data: custom_types.success.SuccessDict and
+            SuccessDict type: `SAVE_SESSION`.
+
+        Raises
+        ------
+        ErrorDictException
+            If data is not a valid SessionDict or session id is not known (cannot update
+            unknown session).
+        """
         # Data check
         if not check_valid_typed_dict(data, SessionDict):
             raise ErrorDictException(
@@ -71,12 +121,33 @@ class Experimenter(_user.User):
         return MessageDict(type="SUCCESS", data=success)
 
     def _handle_delete_session(self, data):
-        """TODO document"""
-        if "session_id" not in data:
+        """Handle requests with type `DELETE_SESSION`.
+
+        Check if data is a valid dict with `session_id`.  If found, delete session with
+        `session_id`.
+
+        Parameters
+        ----------
+        data : any
+            Message data.
+
+        Returns
+        -------
+        custom_types.message.MessageDict
+            MessageDict with type: `SUCCESS`, data: custom_types.success.SuccessDict and
+            SuccessDict type: `DELETE_SESSION`.
+
+        Raises
+        ------
+        ErrorDictException
+            If data is not a valid dict with an `session_id` str or if `session_id` is
+            not known.
+        """
+        if not isinstance(data, dict) or "session_id" not in data:
             raise ErrorDictException(
                 code=400,
                 type="INVALID_REQUEST",
-                description="Missing session_id in request.",
+                description="Request data must be a valid JSON with session_id.",
             )
 
         session_id = data["session_id"]
@@ -88,12 +159,33 @@ class Experimenter(_user.User):
         return MessageDict(type="SUCCESS", data=success)
 
     def _handle_create_experiment(self, data):
-        """TODO document"""
-        if "session_id" not in data:
+        """Handle requests with type `CREATE_EXPERIMENT`.
+
+        Check if data is a valid dict with `session_id`.  If found, try to create a new
+        modules.experiment.Experiment based on the session with `session_id`.
+
+        Parameters
+        ----------
+        data : any
+            Message data.
+
+        Returns
+        -------
+        custom_types.message.MessageDict
+            MessageDict with type: `SUCCESS`, data: custom_types.success.SuccessDict and
+            SuccessDict type: `CREATE_EXPERIMENT`.
+
+        Raises
+        ------
+        ErrorDictException
+            If data is not a valid dict with an `session_id` str, if `session_id` is not
+            known or if there is already an Experiment with `session_id`.
+        """
+        if not isinstance(data, dict) or "session_id" not in data:
             raise ErrorDictException(
                 code=400,
                 type="INVALID_REQUEST",
-                description="Missing session_id in request.",
+                description="Request data must be a valid JSON with session_id.",
             )
 
         self._experiment = self._hub.create_experiment(data["session_id"])
@@ -105,12 +197,33 @@ class Experimenter(_user.User):
         return MessageDict(type="SUCCESS", data=success)
 
     def _handle_join_experiment(self, data):
-        """TODO document"""
-        if "session_id" not in data:
+        """Handle requests with type `JOIN_EXPERIMENT`.
+
+        Check if data is a valid dict with `session_id`.  If found, try to join an
+        existing modules.experiment.Experiment with ID: `session_id`.
+
+        Parameters
+        ----------
+        data : any
+            Message data.
+
+        Returns
+        -------
+        custom_types.message.MessageDict
+            MessageDict with type: `SUCCESS`, data: custom_types.success.SuccessDict and
+            SuccessDict type: `JOIN_EXPERIMENT`.
+
+        Raises
+        ------
+        ErrorDictException
+            If data is not a valid dict with a `session_id` str or if there is no
+            Experiment with `session_id`.
+        """
+        if not isinstance(data, dict) or "session_id" not in data:
             raise ErrorDictException(
                 code=400,
                 type="INVALID_REQUEST",
-                description="Missing session_id in request.",
+                description="Request data must be a valid JSON with session_id.",
             )
 
         experiment = self._hub.experiments.get(data["session_id"])
@@ -131,7 +244,25 @@ class Experimenter(_user.User):
         return MessageDict(type="SUCCESS", data=success)
 
     def _handle_start_experiment(self, _):
-        """TODO document"""
+        """Handle requests with type `START_EXPERIMENT`.
+
+        Parameters
+        ----------
+        _ : any
+            Message data.  Ignored / not required.
+
+        Returns
+        -------
+        custom_types.message.MessageDict
+            MessageDict with type: `SUCCESS`, data: custom_types.success.SuccessDict and
+            SuccessDict type: `START_EXPERIMENT`.
+
+        Raises
+        ------
+        ErrorDictException
+            If this Experimenter is not connected to an modules.experiment.Experiment or
+            the Experiment has already started.
+        """
         if not self._experiment:
             raise ErrorDictException(
                 code=409,
@@ -150,7 +281,25 @@ class Experimenter(_user.User):
         return MessageDict(type="SUCCESS", data=success)
 
     def _handle_stop_experiment(self, _):
-        """TODO document"""
+        """Handle requests with type `STOP_EXPERIMENT`.
+
+        Parameters
+        ----------
+        _ : any
+            Message data.  Ignored / not required.
+
+        Returns
+        -------
+        custom_types.message.MessageDict
+            MessageDict with type: `SUCCESS`, data: custom_types.success.SuccessDict and
+            SuccessDict type: `STOP_EXPERIMENT`.
+
+        Raises
+        ------
+        ErrorDictException
+            If this Experimenter is not connected to an modules.experiment.Experiment or
+            the Experiment has already ended.
+        """
         if not self._experiment:
             raise ErrorDictException(
                 code=409,
@@ -169,7 +318,27 @@ class Experimenter(_user.User):
         return MessageDict(type="SUCCESS", data=success)
 
     def _handle_add_note(self, data):
-        """TODO document"""
+        """Handle requests with type `ADD_NOTE`.
+
+        Check if data is a valid custom_types.note.NoteDict and adds the note to the
+        modules.experiment.Experiment the Experimenter is connected to.
+
+        Parameters
+        ----------
+        _ : any
+            Message data.  Ignored / not required.
+
+        Returns
+        -------
+        custom_types.message.MessageDict
+            MessageDict with type: `SUCCESS`, data: custom_types.success.SuccessDict and
+            SuccessDict type: `ADD_NOTE`.
+
+        Raises
+        ------
+        ErrorDictException
+            If data is not a valid custom_types.note.NoteDict.
+        """
         if not check_valid_typed_dict(data, NoteDict):
             raise ErrorDictException(
                 code=400, type="INVALID_REQUEST", description="Expected note object."
