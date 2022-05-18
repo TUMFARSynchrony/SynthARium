5d9adba8310865918bc5501db79ee0b1b7dcfe92
Alexander Liebald <alexander.liebald@gmail.com>
2022-05-18 15:42:20 +0200

    Renamed Connection.js -> OldConnection.js

--- frontend/src/networking/OldConnection.js
+++ frontend/src/networking/OldConnection.js
@@ -29,7 +29,7 @@ export default class Connection {
     await this.#negotiate();
   }
 
-  stop() {}
+  stop() { }
 
   sendRequest(type, data) {
     const message = JSON.stringify({ type: type, data: data });
