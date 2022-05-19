85bddeb3682adc0eb64927480c7638741066a434
Maria Pospelova <mariyapospelova@gmail.com>
2022-05-19 21:16:25 +0200

    Create heading for watching room

--- frontend/src/pages/WatchingRoom/WatchingRoom.js
+++ frontend/src/pages/WatchingRoom/WatchingRoom.js
@@ -1,11 +1,14 @@
-import LinkButton from "../../components/atoms/LinkButton/LinkButton";
+import { useState } from "react";
+import Heading from "../../components/atoms/Heading/Heading";
 import "./WatchingRoom.css";
 
 function WatchingRoom() {
+  const [state, setState] = useState("WAITING");
   return (
-    <div>
-      <h1>Watching Room</h1>
-      <LinkButton name="Session Overview" to="/" />
+    <div className="watchingRoomContainer">
+      <div className="watchingRoomHeader">
+        <Heading heading={"State: " + state} />
+      </div>
     </div>
   );
 }
