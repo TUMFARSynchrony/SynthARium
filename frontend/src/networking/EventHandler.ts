eab792b8ccfff64480a3411ee070fb3b52623265
Alexander Liebald <alexander.liebald@gmail.com>
2022-05-22 16:40:40 +0200

    Rename variables in EventHandler

--- frontend/src/networking/EventHandler.ts
+++ frontend/src/networking/EventHandler.ts
@@ -1,43 +1,43 @@
 export type EventHandlerFunction<T> = (data: T) => Promise<void>;
 
 export class EventHandler<T> {
-	private handlers: Map<string, EventHandlerFunction<T>[]>;
-	private name: string;
+	private eventHandlers: Map<string, EventHandlerFunction<T>[]>;
+	private handlerName: string;
 	private warnNoHandler: boolean;
 
 	constructor(warnNoHandler: boolean = true, name: string = "EventHandler") {
-		this.handlers = new Map();
-		this.name = name;
+		this.eventHandlers = new Map();
+		this.handlerName = name;
 		this.warnNoHandler = warnNoHandler;
 	}
 
 	public on(event: string, handler: EventHandlerFunction<T>): void {
-		if (this.handlers.has(event)) {
-			this.handlers.get(event).push(handler);
+		if (this.eventHandlers.has(event)) {
+			this.eventHandlers.get(event).push(handler);
 		} else {
-			this.handlers.set(event, [handler]);
+			this.eventHandlers.set(event, [handler]);
 		}
 	}
 
 	public off(event: string, handler: EventHandlerFunction<T>): void {
-		this.handlers.get(event)?.filter(h => h !== handler);
+		this.eventHandlers.get(event)?.filter(h => h !== handler);
 	}
 
 	public removeAllEventHandlers(event?: string): void {
 		if (event) {
 			// remove all handlers for the given event
-			this.handlers.delete(event);
+			this.eventHandlers.delete(event);
 		} else {
 			// Remove all handlers for all events
-			this.handlers.clear();
+			this.eventHandlers.clear();
 		}
 	}
 
 	public async trigger(event: string, data: any): Promise<void> {
-		const handlers = this.handlers.get(event);
+		const handlers = this.eventHandlers.get(event);
 
 		if (this.warnNoHandler && (!handlers || handlers.length === 0)) {
-			console.warn(`[${this.name}] Received event: ${event}, but no handler defined.`);
+			console.warn(`[${this.handlerName}] Received event: ${event}, but no handler defined.`);
 			return;
 		}
 
