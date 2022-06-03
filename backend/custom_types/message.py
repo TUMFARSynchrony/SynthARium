ce0e45dab25b0d338031aed0d9b76d84ecbc36b9
NCEghtebas <NCEghtebas@users.noreply.github.com>
2022-06-03 11:40:17 +0200

    Small spelling error in function description.

--- backend/custom_types/message.py
+++ backend/custom_types/message.py
@@ -11,7 +11,7 @@ import custom_types.util as util
 
 
 class MessageDict(TypedDict):
-    """TypedDict for api messages.  All messages send should be a MessageDict.
+    """TypedDict for api messages.  All messages sent should be a MessageDict.
 
     The `MessageDict` is used to send and receive messages and identify the contents of
     a message via `type`.  The content of a message can be anything, e.g. an ErrorDict,
