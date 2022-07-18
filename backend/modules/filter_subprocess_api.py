f0d74cf66dbec7fc416b29572191825bc2130428
Alexander Liebald <alexander.liebald@gmail.com>
2022-07-18 15:24:51 +0200

    Filter API documentation

--- backend/modules/filter_subprocess_api.py
+++ backend/modules/filter_subprocess_api.py
@@ -1,26 +1,69 @@
-"""TODO document"""
-
+"""Provide `FilterSubprocessAPI` implementation of `FilterAPIInterface`."""
 
 from typing import Callable
 from modules.filter_api_interface import FilterAPIInterface
 
 
 class FilterSubprocessAPI(FilterAPIInterface):
-    """TODO document"""
+    """API enabling filters to access and interact with data on the main process.
+
+    Implements modules.filter_api_interface.FilterAPIInterface
+
+    FilterSubprocessAPI is run on a subprocess and does not have direct access to data
+    / functionality filters may require.  To access that data / functionality, it sends
+    commands to a modules.filter_subprocess_receiver.FilterSubprocessReceiver on the
+    main process.  Sending the commands is  donne using `_relay_command`, wich is
+    intended to be the `_send_command` function in
+    modules.connection_runner.ConnectionRunner.
+
+    Implements modules.filter_api_interface.FilterAPIInterface.
+
+    See Also
+    --------
+    modules.filter_api_interface.FilterAPIInterface : further documentation.
+    https://github.com/TUMFARSynchrony/experimental-hub/wiki/Backend-Architecture
+        Architecture UML Diagram.
+    """
 
     _relay_command: Callable
 
     def __init__(self, relay_command: Callable) -> None:
-        """TODO document"""
+        """Initialize new FilterSubprocessAPI.
+
+        Parameters
+        ----------
+        relay_command : Callable
+            Relay function to send data to
+            modules.filter_subprocess_receiver.FilterSubprocessReceiver on the main
+            process.
+        """
         super().__init__()
         self._relay_command = relay_command
 
     async def experiment_send(self, to: str, data, exclude: str) -> None:
-        """TODO document"""
+        # For docstring see FilterAPIInterface or hover over function declaration
         self._send_command(
             "EXPERIMENT_SEND", {"to": to, "data": data, "exclude": exclude}
         )
 
     def _send_command(self, command: str, data) -> None:
-        """TODO document"""
+        """Send a command to FilterSubprocessReceiver oin the main process.
+
+        Parameters
+        ----------
+        command : str
+            Command identifier used to identify the command in
+            modules.filter_subprocess_receiver.FilterSubprocessReceiver.
+        data : anything json serializable
+            Data associated with the command.
+
+        See Also
+        --------
+        modules.filter_subprocess_receiver.FilterSubprocessReceiver
+            Handle commands on the main process.  Each command send should have a
+            handler in modules.filter_subprocess_receiver.FilterSubprocessReceiver.
+        """
         self._relay_command("FILTER_API", {"command": command, "data": data})
+
+
+FilterAPIInterface.register(FilterSubprocessAPI)
--- backend/modules/filter_subprocess_receiver.py
+++ backend/modules/filter_subprocess_receiver.py
@@ -1,28 +1,58 @@
-"""TODO document"""
-
+"""provide `FilterSubprocessReceiver` class."""
 
 from modules.filter_api import FilterAPI
 
 
 class FilterSubprocessReceiver:
-    """TODO document"""
+    """Receive commands for the FilterAPI from a FilterSubprocessAPI.
+
+    When a modules.filter_subprocess_api.FilterSubprocessAPI wants to execute a API call
+    , it sends a command to a FilterSubprocessReceiver running on the main process.
+    The FilterSubprocessReceiver then forwards the request to a
+    modules.filter_api.FilterAPI.
+
+    See Also
+    --------
+    modules.filter_subprocess_api.FilterSubprocessAPI :
+        FilterSubprocessAPI sending the commands handled by FilterSubprocessReceiver.
+    modules.filter_api_interface.FilterAPIInterface : Filter API documentation.
+    https://github.com/TUMFARSynchrony/experimental-hub/wiki/Backend-Architecture
+        Architecture UML Diagram.
+    """
 
     _filter_api: FilterAPI
 
     def __init__(self, filter_api: FilterAPI) -> None:
-        """TODO document"""
+        """Initialize new FilterSubprocessReceiver.
+
+        Parameters
+        ----------
+        filter_api : modules.filter_api.FilterAPI
+            FilterAPI requests from modules.filter_subprocess_api.FilterSubprocessAPI
+            will be forwarded to.
+        """
         self._filter_api = filter_api
 
     async def handle(self, message: dict):
-        """TODO document"""
+        """Handle a incoming message from a subprocess FilterSubprocessAPI.
+
+        Parameters
+        ----------
+        message : dict
+            Message send by modules.filter_subprocess_api.FilterSubprocessAPI.  There
+            are no guarantees about its content, but the intended content is `command`
+            and `data`.
+        """
         command = message.get("command")
         data = message.get("data")
 
         if command is None or data is None:
+            # TODO handle
             return
 
         match command:
             case "EXPERIMENT_SEND":
+                # TODO handle exception
                 await self._filter_api.experiment_send(
                     data["to"], data["data"], data["exclude"]
                 )
