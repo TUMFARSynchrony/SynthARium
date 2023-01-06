39c0351b0e8ccb39d4c29203eca468261652c24f
KuroiX <geheeb@in.tum.de>
2023-01-06 16:54:01 +0100

    Added documentation to filter_factory and cleaned up naming

--- backend/filters/filter_factory.py
+++ backend/filters/filter_factory.py
@@ -14,41 +14,45 @@ from filters.rotate import RotationFilter
 from modules.exceptions import ErrorDictException
 
 
-def create_filter(filter_config: FilterDict, audio: TrackHandler, video: TrackHandler) -> Filter:
+def create_filter(filter_config: FilterDict,
+                  audio_track_handler: TrackHandler,
+                  video_track_handler: TrackHandler) -> Filter:
     """Create a filter based on `type` of `filter_config`.
 
     Parameters
     ----------
-    id : str
-        Id for new filter.
     filter_config : custom_types.filters.FilterDict
         Filter config used to determine type of filter.  Also passed to new filter.
+    audio_track_handler : TrackHandler
+        The audio TrackHandler to assign
+    video_track_handler : TrackHandler
+        The video TrackHandler to assign
 
     Raises
     ------
     modules.exceptions.ErrorDictException
         If the filter type is unknown.
     """
-    type = filter_config["type"]
+    filter_type = filter_config["type"]
 
-    match type:
+    match filter_type:
         case "MUTE_AUDIO":
-            return MuteAudioFilter(filter_config, audio, video)
+            return MuteAudioFilter(filter_config, audio_track_handler, video_track_handler)
         case "MUTE_VIDEO":
-            return MuteVideoFilter(filter_config, audio, video)
+            return MuteVideoFilter(filter_config, audio_track_handler, video_track_handler)
         case "ROTATION":
-            return RotationFilter(filter_config, audio, video)
+            return RotationFilter(filter_config, audio_track_handler, video_track_handler)
         case "EDGE_OUTLINE":
-            return EdgeOutlineFilter(filter_config, audio, video)
+            return EdgeOutlineFilter(filter_config, audio_track_handler, video_track_handler)
         case "DELAY":
-            return DelayFilter(filter_config, audio, video)  # type: ignore
+            return DelayFilter(filter_config, audio_track_handler, video_track_handler)  # type: ignore
         case "FILTER_API_TEST":
-            return FilterAPITestFilter(filter_config, audio, video)
+            return FilterAPITestFilter(filter_config, audio_track_handler, video_track_handler)
         case "HELLO_WORLD":
-            return HelloWorldFilter(filter_config, audio, video)
+            return HelloWorldFilter(filter_config, audio_track_handler, video_track_handler)
         case _:
             raise ErrorDictException(
                 code=404,
                 type="UNKNOWN_FILTER_TYPE",
-                description=f'Unknown filter type "{type}".',
+                description=f'Unknown filter type "{filter_type}".',
             )
