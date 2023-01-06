231220f53e32f70a041602fd742a3db33e4c1b4a
KuroiX <geheeb@in.tum.de>
2023-01-06 19:54:50 +0100

    Corrected init for previous commit again

--- backend/filters/__init__.py
+++ backend/filters/__init__.py
@@ -1,9 +1,9 @@
 # __all__ = []
-from . import filter_factory
-
 from .filter import Filter
 
 from .api_test import FilterAPITestFilter
 from .edge_outline import EdgeOutlineFilter
 from .rotate import RotationFilter
 from .mute import MuteAudioFilter, MuteVideoFilter
+
+from . import filter_factory
