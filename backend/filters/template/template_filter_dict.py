ddd9a1104ba3adbc0a181ee752445e247765a6eb
Julian Geheeb <geheeb@in.tum.de>
2023-02-11 13:55:46 +0100

    Added template filter

---
+++ backend/filters/template/template_filter_dict.py
@@ -0,0 +1,18 @@
+from filters.filter_dict import FilterDict
+
+
+# TODO: Add or delete this, depending on filters needs
+class TemplateFilterDict(FilterDict):
+    """TypedDict for template filter.
+
+    Attributes
+    ----------
+    type : str
+        filter type (unique identifier / name)
+    id : str
+        Filter id.  Empty string if adding a new filter.  Read only for client.
+    size : int
+        Example value for template
+    """
+
+    size: int
