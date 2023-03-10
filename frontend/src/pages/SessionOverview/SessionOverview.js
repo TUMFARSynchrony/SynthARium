d98e178673b9b0ac8cec6c760594c5b159deeb9b
Ashika08 <manjunathashika@gmail.com>
2023-03-10 13:39:46 +0100

    Refactored the session overview page with mui elements

--- frontend/src/pages/SessionOverview/SessionOverview.js
+++ frontend/src/pages/SessionOverview/SessionOverview.js
@@ -4,15 +4,18 @@ import { useDispatch } from "react-redux";
 import { initializeSession } from "../../features/openSession";
 
 import SessionCard from "../../components/organisms/SessionCard/SessionCard";
-import "./SessionOverview.css";
 import SessionPreview from "../../components/organisms/SessionPreview/SessionPreview";
 import { INITIAL_SESSION_DATA } from "../../utils/constants";
 import { getPastAndFutureSessions } from "../../utils/utils";
-import Label from "../../components/atoms/Label/Label";
 import HeroText from "../../components/atoms/HeroText/HeroText";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import { ActionIconButton, LinkButton } from "../../components/atoms/Button";
+import Typography from "@mui/material/Typography";
+import styled from '@mui/material/styles/styled';
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+
 
 function SessionOverview({
   onDeleteSession,
@@ -51,19 +54,46 @@ function SessionOverview({
     setShowPastSessions(!showPastSessions);
   };
 
+  const WelcomeText = styled(Typography)(({ theme }) => ({
+    margin: theme.spacing(2, 4, 2, 4), // top, right, bottom, left
+    fontStyle: "italic",
+  }));
+
+  const Separator = styled(Divider)(({ theme }) => ({
+    borderRightWidth: 5,
+  }));
+
   return (
     <>
       <HeroText text={"Welcome! Get started with conducting your user studies here!"} />
       <LinkButton text="CREATE NEW SESSION" path="/sessionForm" variant="contained" color="primary" onClick={() => onCreateNewSession()} />
-      <div className="sessionOverviewDescription">
+      <WelcomeText>
         Create a new session to create your own experimental design template.
         You can hold these sessions for each experiment you would like to
         connect a new participant/set of participants to (link to wiki
-        forexperimental workflow).
-      </div>
-      <div className="sessionOverviewContainer">
-        <div className="sessionOverviewCards">
-          <Label title={"Upcoming sessions:"} />
+        for experimental workflow).
+      </WelcomeText>
+      <Grid container>
+        <Grid item sm={7} sx={{ m: 3 }}>
+          {selectedSession ? (
+            <SessionPreview
+              selectedSession={selectedSession}
+              setSelectedSession={setSelectedSession}
+              onDeleteSession={onDeleteSession}
+              onCreateExperiment={onCreateExperiment}
+              onJoinExperiment={onJoinExperiment}
+            />
+          ) : (
+            <h2>No session selected.</h2>
+          )}
+        </Grid>
+        <Grid item>
+          <Separator orientation="vertical" />
+        </Grid>
+        <Grid item sm={4} sx={{ m: 3 }}>
+          <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+            Upcoming sessions:
+          </Typography>
           {future.length !== 0 ? (
             future.map((session, index) => {
               return (
@@ -80,10 +110,10 @@ function SessionOverview({
           ) : (
             <>No active sessions found.</>
           )}
-          <ActionIconButton text={showPastSessions ? "Hide past sessions" : "Show past sessions"} 
-            variant="outlined" 
+          <ActionIconButton text={showPastSessions ? "Hide past sessions" : "Show past sessions"}
+            variant="outlined"
             color="primary"
-            onClick={() => onShowPastSessions()} 
+            onClick={() => onShowPastSessions()}
             icon={showPastSessions ? <ExpandLess /> : <ExpandMore />} />
           {showPastSessions &&
             past.length > 0 &&
@@ -99,21 +129,8 @@ function SessionOverview({
                 />
               );
             })}
-        </div>
-        <>
-          {selectedSession ? (
-            <SessionPreview
-              selectedSession={selectedSession}
-              setSelectedSession={setSelectedSession}
-              onDeleteSession={onDeleteSession}
-              onCreateExperiment={onCreateExperiment}
-              onJoinExperiment={onJoinExperiment}
-            />
-          ) : (
-            <h2>No session selected.</h2>
-          )}
-        </>
-      </div>
+        </Grid>
+      </Grid>
     </>
   );
 }
