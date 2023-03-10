6bb5dae67aa23013f31bfb45c1303657a8954022
Ashika08 <manjunathashika@gmail.com>
2023-03-10 13:37:33 +0100

    Refactored upcoming session cards with mui

--- frontend/src/components/organisms/SessionCard/SessionCard.js
+++ frontend/src/components/organisms/SessionCard/SessionCard.js
@@ -1,20 +1,35 @@
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Box from "@mui/material/Box";
+import CardActionArea from "@mui/material/CardActionArea";
+import Stack from "@mui/material/Stack";
 import { integerToDateTime } from "../../../utils/utils";
-import "./SessionCard.css";
 
 function SessionCard({ title, date, description, onClick, selected }) {
   return (
-    <div
-      className={
-        !selected ? "sessionCardContainer" : "sessionCardContainer active"
-      }
-      onClick={onClick}
-    >
-      <div className="sessionCardHeader">
-        <h3 className="sessionCardHeaderTitle">{title}</h3>
-        <h3 className="sessionCardHeaderDate">{integerToDateTime(date)}</h3>
-      </div>
-      <p className="sessionMetaInformation">{description}</p>
-    </div>
+    <Card sx={{ mt: 2, mb: 2, borderTop: '3px solid dodgerblue' }} >
+      <CardActionArea onClick={onClick}>
+        <CardContent>
+          <Stack spacing={1}>
+            <Typography component='div' variant="subtitle1" align="left" sx={{ color: 'grey', fontWeight: 'bold' }}>
+              {title}
+            </Typography>
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+              <Typography>
+                {integerToDateTime(date).toLocaleDateString()}
+              </Typography>
+              <Typography>
+                {integerToDateTime(date).toLocaleTimeString()}
+              </Typography>
+            </Box>
+            <Typography align="left" noWrap="true">
+              {description}
+            </Typography>
+          </Stack>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 }
 
