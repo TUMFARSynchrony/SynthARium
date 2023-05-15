b0c2073073c2c1984643654ede3daf7060afc470
Julian Geheeb <geheeb@in.tum.de>
2023-05-15 14:00:48 +0200

    Updated template filter with SimpleLineWriter

--- backend/filters/template/template_filter.py
+++ backend/filters/template/template_filter.py
@@ -5,6 +5,7 @@ import numpy
 
 # from custom_types import util
 from filters import Filter
+from filters.simple_line_writer import SimpleLineWriter
 
 
 # from filters.template.template_filter_dict import TemplateFilterDict
@@ -13,6 +14,11 @@ from filters import Filter
 class TemplateFilter(Filter):
     """A simple example filter printing `Hello World` on a video Track.
     Can be used to as a template to copy when creating an own filter."""
+    line_writer: SimpleLineWriter
+
+    def __init__(self, config, audio_track_handler, video_track_handler):
+        super().__init__(config, audio_track_handler, video_track_handler)
+        self.line_writer = SimpleLineWriter()
 
     @staticmethod
     def name(self) -> str:
@@ -21,14 +27,7 @@ class TemplateFilter(Filter):
 
     async def process(self, _, ndarray: numpy.ndarray) -> numpy.ndarray:
         # TODO: change this to implement filter
-        # Parameters for cv2.putText
-        origin = (50, 50)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        font_size = 1
-        color = (0, 0, 0)
-
-        # Put text on image
-        ndarray = cv2.putText(ndarray, "Hello World", origin, font, font_size, color)
+        self.line_writer.write_line(ndarray, "Hello World")
 
         # Return modified frame
         return ndarray
