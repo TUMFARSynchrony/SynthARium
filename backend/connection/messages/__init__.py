717ad9137a0d456f4397a558eb7f487b8e92d658
Julian Geheeb <geheeb@in.tum.de>
2023-05-21 17:58:29 +0200

    Changed ordering of imports

--- backend/connection/messages/__init__.py
+++ backend/connection/messages/__init__.py
@@ -3,13 +3,14 @@
 Use for type hints and static type checking without any overhead during runtime.
 """
 
-from .connection_answer_dict import (
-    ConnectionAnswerDict,
-    is_valid_connection_answer_dict,
-)
-from .connection_offer_dict import ConnectionOfferDict, is_valid_connection_offer_dict
 from .connection_proposal_dict import ConnectionProposalDict
 from .rtc_session_description_dict import (
     RTCSessionDescriptionDict,
     is_valid_rtc_session_description_dict,
 )
+from .connection_answer_dict import (
+    ConnectionAnswerDict,
+    is_valid_connection_answer_dict,
+)
+from .connection_offer_dict import ConnectionOfferDict, is_valid_connection_offer_dict
+
