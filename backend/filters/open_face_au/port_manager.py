1b31f14445392dbb167d0e40ad8b23648a26db20
aykutaaykut <aykutaaykut.ai@gmail.com>
2023-06-20 17:40:44 +0200

    path and variable names are corrected

--- backend/filters/open_face_au/port_manager.py
+++ backend/filters/open_face_au/port_manager.py
@@ -18,15 +18,15 @@ class PortManager:
     port: int
 
     def __init__(self):
-        self._lock = NamedAtomicLock("open_face_port", os.path.dirname(__file__))
+        self._lock = NamedAtomicLock("open_face_port", os.path.dirname(os.path.abspath(__file__)))
         self._lock.acquire()
 
         try:
-            self._shared_port: shared_memory.SharedMemory = shared_memory.SharedMemory(
+            self._shared_port_slots: shared_memory.SharedMemory = shared_memory.SharedMemory(
                 name="open_face_port", create=False, size=self._size
             )
         except:
-            self._shared_port: shared_memory.SharedMemory = shared_memory.SharedMemory(
+            self._shared_port_slots: shared_memory.SharedMemory = shared_memory.SharedMemory(
                 name="open_face_port", create=True, size=self._size
             )
             self._write_to_shared_memory(0)
@@ -46,16 +46,16 @@ class PortManager:
         next_slots = current_slots - (1 << self._slot)
         self._write_to_shared_memory(next_slots)
 
-        self._shared_port.close()
+        self._shared_port_slots.close()
         if next_slots == 0:
-            self._shared_port.unlink()
+            self._shared_port_slots.unlink()
 
         self._lock.release()
 
     def _write_to_shared_memory(self, value: int):
-        self._shared_port.buf[: self._size] = bytearray(
+        self._shared_port_slots.buf[: self._size] = bytearray(
             value.to_bytes(self._size, "big")
         )
 
-    def _read_from_shared_memory(self):
-        return int.from_bytes(bytes(self._shared_port.buf[: self._size]), "big")
+    def _read_from_shared_memory(self) -> int:
+        return int.from_bytes(bytes(self._shared_port_slots.buf[: self._size]), "big")
