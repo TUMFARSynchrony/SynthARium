cdc3feb10805e66e3926dbb9dcfd8a09eec55f46
mertyldrr <mertyildirir14@gmail.com>
2023-06-30 21:58:42 +0200

    add enums

---
+++ frontend/src/utils/enums.ts
@@ -0,0 +1,17 @@
+export enum ExperimentTimes {
+  CREATION_TIME = "creation_time",
+  START_TIME = "start_time",
+  END_TIME = "end_time"
+}
+
+export enum BanMuteUnmuteActions {
+  MUTED_AUDIO = "muted_audio",
+  MUTED_VIDEO = "muted_video"
+}
+
+export enum ChangeSessionAttribute {
+  TITLE = "title",
+  DESCRIPTION = "description",
+  RECORD = "record",
+  DATE = "date"
+}
