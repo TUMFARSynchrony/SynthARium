81a34e33ba0201c6c87c28c3744113d93f945563
mertyldrr <mertyildirir14@gmail.com>
2023-07-14 03:54:28 +0300

    convert ActionButton to ts

---
+++ frontend/src/components/atoms/Button/ActionButton.tsx
@@ -0,0 +1,28 @@
+import Button, { ButtonProps } from "@mui/material/Button";
+
+interface ActionButtonProps extends ButtonProps {
+  text: string;
+}
+
+function ActionButton({
+  text,
+  variant,
+  color,
+  size,
+  onClick,
+  disabled = false
+}: ActionButtonProps) {
+  return (
+    <Button
+      variant={variant}
+      color={color}
+      size={size}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {text}
+    </Button>
+  );
+}
+
+export default ActionButton;
