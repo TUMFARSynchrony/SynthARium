10a088d75b41c70d9cd6147efdf47931c9714d81
mertyldrr <mertyildirir14@gmail.com>
2023-07-14 03:41:05 +0300

    convert LinkButton to ts

---
+++ frontend/src/components/atoms/Button/LinkButton.tsx
@@ -0,0 +1,24 @@
+import Button, { ButtonProps } from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+
+interface LinkButtonProps extends ButtonProps {
+  text: string;
+  path: string;
+  onClick: () => void;
+}
+
+function LinkButton({ text, variant, size, path }: LinkButtonProps) {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate(path);
+  };
+
+  return (
+    <Button variant={variant} size={size} onClick={handleButtonClick}>
+      {text}
+    </Button>
+  );
+}
+
+export default LinkButton;
