b87d611fb0aef9b705045279ef95ead5463a1128
mertyldrr <mertyildirir14@gmail.com>
2023-07-14 03:40:44 +0300

    convert LinkActionButton to ts

---
+++ frontend/src/components/atoms/Button/LinkActionButton.tsx
@@ -0,0 +1,39 @@
+import Button, { ButtonProps } from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+
+interface LinkActionButtonProps extends ButtonProps {
+  text: string;
+  path: string;
+  onClick: () => void;
+}
+
+function LinkActionButton({
+  text,
+  variant,
+  path,
+  size,
+  color,
+  onClick
+}: LinkActionButtonProps) {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate(path);
+  };
+
+  return (
+    <Button
+      variant={variant}
+      size={size}
+      color={color}
+      onClick={() => {
+        handleButtonClick();
+        onClick();
+      }}
+    >
+      {text}
+    </Button>
+  );
+}
+
+export default LinkActionButton;
