fd5e79e25c130716653ea07b017f54f7ec98f3b9
aykutaaykut <aykutaaykut.ai@gmail.com>
2023-07-31 14:43:02 +0200

    fixed error caused by stdout in pipeline and removed previous fix with flush

--- backend/filters/open_face_au/open_face.py
+++ backend/filters/open_face_au/open_face.py
@@ -35,10 +35,3 @@ class OpenFace:
             self._openface_process.terminate()
         except Exception:
             self._openface_process.kill()
-
-    def flush_result(self):
-        # This is a bit ugly/hacky, but it stops the stdout from overflowing
-        print(self._openface_process.stdout.readline())
-        print(self._openface_process.stdout.readline())
-        print(self._openface_process.stdout.readline())
-        print(self._openface_process.stdout.readline())
