455a5f9dfce7a9bccf60b59c92feed0b61c905f2
aykutaaykut <aykutaaykut.ai@gmail.com>
2023-09-21 14:05:14 +0200

    updated __init__ file in group_filters package to automatically import all submodules

--- backend/group_filters/__init__.py
+++ backend/group_filters/__init__.py
@@ -3,4 +3,10 @@ from group_filters.group_filter_aggregator import GroupFilterAggregator
 from group_filters import group_filter_factory
 from group_filters import group_filter_utils
 
-from group_filters.template import TemplateGroupFilter
+import pkgutil
+
+__all__ = []
+for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
+    __all__.append(module_name)
+    _module = loader.find_module(module_name).load_module(module_name)
+    globals()[module_name] = _module
