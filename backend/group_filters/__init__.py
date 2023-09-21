2d60a37cfbb10aa090398ebc9802d06618c29eb3
aykutaaykut <aykutaaykut.ai@gmail.com>
2023-09-21 17:53:54 +0200

    manually add gfs in the __init__ file

--- backend/group_filters/__init__.py
+++ backend/group_filters/__init__.py
@@ -3,10 +3,4 @@ from group_filters.group_filter_aggregator import GroupFilterAggregator
 from group_filters import group_filter_factory
 from group_filters import group_filter_utils
 
-import pkgutil
-
-__all__ = []
-for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
-    __all__.append(module_name)
-    _module = loader.find_module(module_name).load_module(module_name)
-    globals()[module_name] = _module
+from group_filters.template import TemplateGroupFilter
