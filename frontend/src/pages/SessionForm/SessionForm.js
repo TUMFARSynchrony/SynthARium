188edb59cc94c9c2e8e45f6eb339e2cdd2369611
mertyldrr <mertyildirir14@gmail.com>
2023-10-02 00:10:24 +0200

    place participants on a new row if participant count is more than 20

--- frontend/src/pages/SessionForm/SessionForm.js
+++ frontend/src/pages/SessionForm/SessionForm.js
@@ -1,6 +1,6 @@
 import DragAndDrop from "../../components/organisms/DragAndDrop/DragAndDrop";
 import ParticipantData from "../../components/organisms/ParticipantData/ParticipantData";
-import { CANVAS_SIZE, INITIAL_PARTICIPANT_DATA } from "../../utils/constants";
+import { INITIAL_PARTICIPANT_DATA } from "../../utils/constants";
 import {
   checkValidSession,
   filterListByIndex,
@@ -13,12 +13,9 @@ import AddIcon from "@mui/icons-material/Add";
 import ChevronLeft from "@mui/icons-material/ChevronLeft";
 import ChevronRight from "@mui/icons-material/ChevronRight";
 import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import { useEffect, useState } from "react";
@@ -57,6 +54,8 @@ function SessionForm({ onSendSessionToBackend }) {
       sessionData.participants ? sessionData.participants : []
     )
   );
+
+  console.log(numberOfParticipants);
 
   // It is used as flags to display warning notifications upon entry of incorrect data/not saved in the Participant Modal.
   // It is displayed here instead of in the Participant Modal itself, since upon closing the modal it is no longer
@@ -108,9 +107,14 @@ function SessionForm({ onSendSessionToBackend }) {
     }
   }, [snackbarResponse]);
 
-  const handleCanvasPlacement = () => {
-    setXAxis(xAxis + 25);
-    setYAxis(yAxis + 25);
+  const handleCanvasPlacement = (participantCount) => {
+    if (participantCount !== 0 && participantCount % 20 === 0) {
+      setXAxis(0);
+      setYAxis(Math.ceil(participantCount / 20) * 25);
+    } else {
+      setXAxis(xAxis + 25);
+      setYAxis(yAxis + 25);
+    }
   };
 
   const onDeleteParticipant = (index) => {
@@ -119,7 +123,12 @@ function SessionForm({ onSendSessionToBackend }) {
   };
 
   const onAddParticipant = () => {
-    dispatch(addParticipant(INITIAL_PARTICIPANT_DATA));
+    dispatch(
+      addParticipant({
+        ...INITIAL_PARTICIPANT_DATA,
+        position: { x: xAxis, y: yAxis }
+      })
+    );
     const newParticipantDimensions = [
       ...participantDimensions,
       {
@@ -210,117 +219,107 @@ function SessionForm({ onSendSessionToBackend }) {
     setParticipantDimensions(dimensions);
   };
 
-  // TO DO: auto create x no. of participants.
-  // const handleCreateParticipants = () => {
-  //   for (let i = 0; i < numOfParticipants; i++) {
-  //     onAddParticipant();
-  //     console.log(sessionData);
-  //   }
-  // };
   return (
     <>
-      <Grid container sx={{ mx: 4, my: 2 }}>
+      <div className="flex flex-row h-full px-4 py-8 items-center">
         {showSessionDataForm && (
-          <Grid item sm={5}>
-            <Box sx={{ display: "flex", justifyContent: "flex-start", mb: 1 }}>
-              <Box sx={{ display: "flex", alignItems: "center" }}>
+          <div className="shadow-lg rounded-md h-full">
+            <div className="px-4 flex flex-col h-full">
+              <div className="flex justify-start items-center">
                 <ChevronLeft sx={{ color: "gray" }} />
-              </Box>
-              <LinkButton
-                text="Back to Session Overview"
-                variant="text"
-                size="small"
-                path="/"
-              />
-            </Box>
-            <Card elevation={3}>
-              <Typography variant="h6" sx={{ mt: 2, fontWeight: "bold" }}>
-                Session Details
-              </Typography>
-              <Box sx={{ height: "74vh", overflowY: "auto" }}>
-                <CardContent>
-                  {/* Override text fields' margin and width using MUI classnames */}
-                  <Box
-                    component="form"
-                    sx={{ "& .MuiTextField-root": { m: 1 } }}
-                    noValidate
-                    autoComplete="off"
-                  >
-                    <Box sx={{ "& .MuiTextField-root": { width: "38vw" } }}>
-                      <TextField
-                        label="Title"
-                        value={sessionData.title}
-                        size="small"
-                        required
-                        onChange={(event) =>
-                          handleSessionDataChange("title", event.target.value)
-                        }
-                      />
-                      <TextField
-                        label="Description"
-                        value={sessionData.description}
-                        size="small"
-                        required
-                        onChange={(event) =>
-                          handleSessionDataChange(
-                            "description",
-                            event.target.value
-                          )
-                        }
-                      />
-                    </Box>
-                    <Box sx={{ "& .MuiTextField-root": { width: "18.5vw" } }}>
-                      <TextField
-                        value={
-                          sessionData.date ? formatDate(sessionData.date) : ""
-                        }
-                        type="datetime-local"
-                        size="small"
-                        required
-                        onChange={(event) =>
-                          handleSessionDataChange(
-                            "date",
-                            event.target.value
-                              ? new Date(event.target.value).getTime()
-                              : 0
-                          )
-                        }
-                      />
-                      <TextField
-                        label="Number of Participants"
-                        value={numberOfParticipants}
-                        type="number"
-                        size="small"
-                        disabled
-                      />
-                    </Box>
-                    <Box sx={{ mt: 1, mb: 3 }}>
-                      <FormControlLabel
-                        control={<Checkbox />}
-                        label="Record Session"
-                        checked={sessionData.record}
-                        onChange={() =>
-                          handleSessionDataChange("record", !sessionData.record)
-                        }
-                      />
-                      {/* <ActionIconButton text="Create participants" variant="contained" color="primary" size="small" onClick={() => handleCreateParticipants()} icon={<PeopleOutline />} /> */}
-                    </Box>
-                  </Box>
-
-                  <Box sx={{ display: "flex", justifyContent: "center" }}>
-                    <Typography variant="h6" sx={{ my: 1, fontWeight: "bold" }}>
-                      Participants
-                    </Typography>
-                    <ActionIconButton
-                      text="ADD"
-                      variant="outlined"
-                      color="primary"
+                <LinkButton
+                  text="Back to Session Overview"
+                  variant="text"
+                  size="small"
+                  path="/"
+                />
+              </div>
+              <CardContent>
+                {/* Override text fields' margin and width using MUI classnames */}
+                <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                  Session Details
+                </Typography>
+                <Box
+                  component="form"
+                  sx={{ "& .MuiTextField-root": { m: 1 } }}
+                  noValidate
+                  autoComplete="off"
+                >
+                  <Box sx={{ "& .MuiTextField-root": { width: "38vw" } }}>
+                    <TextField
+                      label="Title"
+                      value={sessionData.title}
                       size="small"
-                      onClick={() => onAddParticipant()}
-                      icon={<AddIcon />}
+                      required
+                      onChange={(event) =>
+                        handleSessionDataChange("title", event.target.value)
+                      }
+                    />
+                    <TextField
+                      label="Description"
+                      value={sessionData.description}
+                      size="small"
+                      required
+                      onChange={(event) =>
+                        handleSessionDataChange(
+                          "description",
+                          event.target.value
+                        )
+                      }
                     />
                   </Box>
+                  <Box sx={{ "& .MuiTextField-root": { width: "18.5vw" } }}>
+                    <TextField
+                      value={
+                        sessionData.date ? formatDate(sessionData.date) : ""
+                      }
+                      type="datetime-local"
+                      size="small"
+                      required
+                      onChange={(event) =>
+                        handleSessionDataChange(
+                          "date",
+                          event.target.value
+                            ? new Date(event.target.value).getTime()
+                            : 0
+                        )
+                      }
+                    />
+                    <TextField
+                      label="Number of Participants"
+                      value={numberOfParticipants}
+                      type="number"
+                      size="small"
+                      disabled
+                    />
+                  </Box>
+                  <Box sx={{ mt: 1, mb: 3 }}>
+                    <FormControlLabel
+                      control={<Checkbox />}
+                      label="Record Session"
+                      checked={sessionData.record}
+                      onChange={() =>
+                        handleSessionDataChange("record", !sessionData.record)
+                      }
+                    />
+                    {/* <ActionIconButton text="Create participants" variant="contained" color="primary" size="small" onClick={() => handleCreateParticipants()} icon={<PeopleOutline />} /> */}
+                  </Box>
+                </Box>
 
+                <div className="flex">
+                  <Typography variant="h6" sx={{ my: 1, fontWeight: "bold" }}>
+                    Participant List
+                  </Typography>
+                  <ActionIconButton
+                    text="ADD"
+                    variant="outlined"
+                    color="primary"
+                    size="small"
+                    onClick={() => onAddParticipant()}
+                    icon={<AddIcon />}
+                  />
+                </div>
+                <div className="overflow-y-scroll h-[300px] shadow-lg bg-slate-50 pl-4 py-2">
                   {openSession.participants.map((participant, index) => {
                     return (
                       <ParticipantData
@@ -335,22 +334,23 @@ function SessionForm({ onSendSessionToBackend }) {
                       />
                     );
                   })}
-                </CardContent>
-              </Box>
-              <Box sx={{ my: 1 }}>
-                {/* <ActionButton text="RANDOM SESSION DATA" variant="contained" color="primary" size="medium" onClick={() => addRandomSessionData()} /> */}
-                <ActionButton
-                  text="SAVE SESSION"
-                  variant="contained"
-                  color="success"
-                  size="medium"
-                  onClick={() => onSaveSession()}
-                />
-              </Box>
-            </Card>
-          </Grid>
+                </div>
+              </CardContent>
+              <div className="flex justify-center h-full pb-2">
+                <div className="self-end">
+                  <ActionButton
+                    text="SAVE SESSION"
+                    variant="contained"
+                    color="success"
+                    size="medium"
+                    onClick={() => onSaveSession()}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         )}
-        <Grid item>
+        <div>
           <ActionIconButton
             text=""
             variant="text"
@@ -359,28 +359,14 @@ function SessionForm({ onSendSessionToBackend }) {
             onClick={() => onShowSessionFormModal()}
             icon={showSessionDataForm ? <ChevronLeft /> : <ChevronRight />}
           />
-        </Grid>
-        <Grid item sm={5}>
-          <Box sx={{ display: "flex", justifyContent: "flex-start" }}>
-            {/* <Link variant="body2" sx={{ color: "gray", textDecorationColor: "gray", fontWeight: "bold" }}>
-              Expand Video Canvas
-            </Link> */}
-          </Box>
-          <Paper
-            elevation={2}
-            sx={{
-              backgroundColor: "whitesmoke",
-              width: CANVAS_SIZE.width,
-              height: CANVAS_SIZE.height
-            }}
-          >
-            <DragAndDrop
-              participantDimensions={participantDimensions}
-              setParticipantDimensions={setParticipantDimensions}
-            />
-          </Paper>
-        </Grid>
-      </Grid>
+        </div>
+        <div className="w-full h-full bg-slate-200 rounded-md shadow-lg">
+          <DragAndDrop
+            participantDimensions={participantDimensions}
+            setParticipantDimensions={setParticipantDimensions}
+          />
+        </div>
+      </div>
       <CustomSnackbar
         open={snackbar.open}
         text={snackbar.text}
