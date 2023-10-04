9ec1f61c76ab2452bd5b7c09725422f8336d8398
aykutaaykut <aykutaaykut.ai@gmail.com>
2023-10-04 23:12:52 +0200

    updated template group filter with a static align_data method and added comments in order to guide developers with the new group filter creation steps

--- backend/group_filters/__init__.py
+++ backend/group_filters/__init__.py
@@ -3,4 +3,5 @@ from group_filters.group_filter_aggregator import GroupFilterAggregator
 from group_filters import group_filter_factory
 from group_filters import group_filter_utils
 
+# TODO: Import the new group filter at the end to add it to the group_filters namespace
 from group_filters.template import TemplateGroupFilter
--- backend/group_filters/template/template_group_filter.py
+++ backend/group_filters/template/template_group_filter.py
@@ -6,28 +6,41 @@ from typing import Any, Callable
 from scipy.interpolate import interp1d
 
 
-def custom_align_fn(x: list, y: list, **kwargs: dict) -> Callable:
-    return interp1d(x, y, **kwargs)
-
-
 class TemplateGroupFilter(GroupFilter):
-    data_len_per_participant = 1
-    num_participants_in_aggregation = 2
-    align_fn = custom_align_fn
-    align_fn_kwargs = {"kind": "nearest", "fill_value": "extrapolate"}
+    """
+    A simple template group filter which applies the followings:
+    - takes the mean of each frame on a video track of a participant at the individual
+    frame processing step.
+    - takes the standard deviation of aligned frames of 2 participants at the
+    aggregation step.
+    Can be used as a template to copy when creating a new group filter.
+    """
+
+    data_len_per_participant = 1  # data required for aggreagtion
+    num_participants_in_aggregation = 2  # number of participants joining in aggregation
 
     def __init__(self, config: FilterDict, participant_id: str):
         super().__init__(config, participant_id)
 
     @staticmethod
     def name() -> str:
+        # TODO: Change this name to a unique name.
         return "TEMPLATE_GF"
 
     async def process_individual_frame(
         self, original: VideoFrame | AudioFrame, ndarray: np.ndarray
     ) -> Any:
+        # TODO: Change this to implement the individual frame processing step.
         return ndarray.mean()
 
+    def align_data(x: list, y: list, base_timeline: list) -> list:
+        # TODO: Change this to implement an alignment function.
+        # Needs to be implemented as a static method.
+        interpolator = interp1d(x, y, kind="nearest", fill_value="extrapolate")
+        return list(interpolator(base_timeline))
+
     def aggregate(data: list[list[Any]]) -> Any:
+        # TODO: Change this to implement the aggregation step.
+        # Needs to be implemented as a static method.
         np_data = np.array(data)
-        return np_data.mean(axis=0)
+        return np_data.std(axis=0)
