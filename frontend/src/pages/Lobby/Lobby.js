165a782d47383b0fdfeb7c22da5fccd2f0f664b9
Berke Atropat <berke.atropat@produktmacher.com>
2023-10-16 15:21:23 +0200

    fix: delete userConsent checks from lobby

--- frontend/src/pages/Lobby/Lobby.js
+++ frontend/src/pages/Lobby/Lobby.js
@@ -12,7 +12,6 @@ import "./Lobby.css";
 
 function Lobby({ localStream, connection, onGetSession, onChat }) {
   const videoElement = useRef(null);
-  const [userConsent, setUserConsent] = useState(false);
   const [connectionState, setConnectionState] = useState(null);
   const [connectedParticipants, setConnectedParticipants] = useState([]);
   const sessionData = useAppSelector(selectCurrentSession);
@@ -45,16 +44,14 @@ function Lobby({ localStream, connection, onGetSession, onChat }) {
     };
   }, [connection]);
   useEffect(() => {
-    if (userConsent) {
-      setParticipantStream(localStream);
-    }
-  }, [localStream, userConsent]);
+    setParticipantStream(localStream);
+  }, [localStream]);
 
   useEffect(() => {
-    if (participantStream && userConsent && videoElement.current) {
+    if (participantStream && videoElement.current) {
       videoElement.current.srcObject = localStream;
     }
-  }, [localStream, participantStream, userConsent]);
+  }, [localStream, participantStream]);
 
   const streamChangeHandler = async () => {
     console.log("%cRemote Stream Change Handler", "color:blue");
@@ -69,26 +66,28 @@ function Lobby({ localStream, connection, onGetSession, onChat }) {
       {/* Grid takes up screen space left from the AppToolbar */}
       <div className="flex h-[calc(100vh-84px)]">
         <div className="px-6 py-4 w-3/4 flex flex-col">
-          {userConsent ? (
-            participantStream ? (
-              sessionData && connectedParticipants ? (
-                <VideoCanvas
-                  connectedParticipants={connectedParticipants}
-                  sessionData={sessionData}
-                  localStream={localStream}
-                  ownParticipantId={participantIdParam}
-                />
-              ) : (
-                <video ref={videoElement} autoPlay playsInline width="100%" height="100%"></video>
-              )
+          {participantStream ? (
+            sessionData && connectedParticipants ? (
+              <VideoCanvas
+                connectedParticipants={connectedParticipants}
+                sessionData={sessionData}
+                localStream={localStream}
+                ownParticipantId={participantIdParam}
+              />
             ) : (
-              <Typography>
-                Unable to access your video. Please check that you have allowed access to your
-                camera and microphone.
-              </Typography>
+              <video
+                ref={videoElement}
+                autoPlay
+                playsInline
+                width="100%"
+                height="100%"
+              ></video>
             )
           ) : (
-            <Typography>Please check if you gave your consent!</Typography>
+            <Typography>
+              Unable to access your video. Please check that you have allowed
+              access to your camera and microphone.
+            </Typography>
           )}
         </div>
         <div className="w-1/4">
