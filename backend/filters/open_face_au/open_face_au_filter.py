36648805bcd87365b5010b42f0846f1c4775e218
normaps <normapus1@gmail.com>
2023-10-30 01:21:46 +0100

    Update open face publisher

--- backend/filters/open_face_au/open_face_au_filter.py
+++ backend/filters/open_face_au/open_face_au_filter.py
@@ -1,9 +1,10 @@
+import cv2
 import numpy
 from av import VideoFrame
 
 from filters.filter import Filter
 from filters.simple_line_writer import SimpleLineWriter
-from filters.open_face_au.open_face_au_extractor import OpenFaceAUExtractor
+from filters.open_face_au.open_face_publisher import OpenFacePublisher
 from .open_face_data_parser import OpenFaceDataParser
 
 
@@ -14,11 +15,11 @@ class OpenFaceAUFilter(Filter):
     data: dict
     file_writer: OpenFaceDataParser
     line_writer: SimpleLineWriter
-    au_extractor: OpenFaceAUExtractor
+    publisher: OpenFacePublisher
 
     def __init__(self, config, audio_track_handler, video_track_handler):
         super().__init__(config, audio_track_handler, video_track_handler)
-        self.au_extractor = OpenFaceAUExtractor()
+        self.publisher = OpenFacePublisher()
         self.line_writer = SimpleLineWriter()
         self.file_writer = OpenFaceDataParser()
 
@@ -26,7 +27,7 @@ class OpenFaceAUFilter(Filter):
         self.frame = 0
 
     def __del__(self):
-        del self.file_writer, self.line_writer, self.au_extractor
+        del self.file_writer, self.line_writer, self.publisher
 
     @staticmethod
     def name(self) -> str:
@@ -54,22 +55,23 @@ class OpenFaceAUFilter(Filter):
         self, original: VideoFrame, ndarray: numpy.ndarray
     ) -> numpy.ndarray:
         self.frame = self.frame + 1
-        exit_code, msg, result = self.au_extractor.extract(ndarray)
+        success, image = cv2.imencode(".png", ndarray)
+        response = self.publisher.send(image.tostring())
 
-        if exit_code == 0:
-            self.data = result
-            # TODO: use correct frame
-            # if a frame is skipped, data corresponds to a frame before current frame, but self.frame does not
-            self.file_writer.write(self.frame, self.data)
-        else:
-            self.file_writer.write(self.frame, {"intensity": "-1"})
+        # if exit_code == 0:
+        #     self.data = result
+        #     # TODO: use correct frame
+        #     # if a frame is skipped, data corresponds to a frame before current frame, but self.frame does not
+        #     self.file_writer.write(self.frame, self.data)
+        # else:
+        #     self.file_writer.write(self.frame, {"intensity": "-1"})
 
-        # Put text on image
-        au06 = self.data["intensity"]["AU06"]
-        au12 = self.data["intensity"]["AU12"]
-        ndarray = self.line_writer.write_lines(
-            ndarray, [f"AU06: {au06}", f"AU12: {au12}", msg]
-        )
+        # # Put text on image
+        # au06 = self.data["intensity"]["AU06"]
+        # au12 = self.data["intensity"]["AU12"]
+        # ndarray = self.line_writer.write_lines(
+        #     ndarray, [f"AU06: {au06}", f"AU12: {au12}", msg]
+        # )
         return ndarray
 
     async def cleanup(self) -> None:
---
+++ backend/filters/open_face_au/open_face_publisher.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+from time import sleep
+import logging
+import pika
+import threading
+import uuid
+
+
+class OpenFacePublisher():
+
+    """Asynchronous Rpc client."""
+    # internal_lock = threading.Lock()
+    queue = {}
+
+    def __init__(self):
+
+        self.logger = logging.getLogger("OpenFacePublisher")
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+
+        self.channel = self.connection.channel()
+        queue_name = "test"
+        try:
+            # Attempt to declare an exclusive queue
+            result = self.channel.queue_declare(queue='test', exclusive=True)
+            print(f"Declared exclusive queue: {queue_name}")
+        except pika.exceptions.ChannelClosedByBroker as e:
+            if "RESOURCE_LOCKED" in str(e):
+                print(f"Queue '{queue_name}' is already declared as exclusive on another connection.")
+                self.channel.queue_bind
+            else:
+                raise
+        
+        self.callback_queue = queue_name
+        self.logger.debug(
+            f"Declaring queue: {self.callback_queue}"
+        )
+
+        self.channel.basic_consume(
+            queue=self.callback_queue,
+            on_message_callback=self.on_response,
+            auto_ack=True)
+
+        self.response = None
+        self.corr_id = None
+        # thread = threading.Thread(target=self.process_data_events)
+        # thread.setDaemon(True)
+        # thread.start()
+    
+    def process_data_events(self):
+        """Check for incoming data events.
+
+        We do this on a thread to allow the flask instance to send
+        asynchronous requests.
+
+        It is important that we lock the thread each time we check for events.
+        """
+        self.channel.basic_consume(queue=self.callback_queue,
+                                   on_message_callback=self.on_response, 
+                                   auto_ack=False)
+        while True:
+            with self.internal_lock:
+                self.connection.process_data_events(time_limit=None)
+                sleep(0.1)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def send(self, message):
+        self.response = None
+        try:
+            self.corr_id = str(uuid.uuid4())
+            self.channel.basic_publish(
+                exchange='amq.direct',
+                routing_key='test',
+                properties=pika.BasicProperties(
+                    reply_to=self.callback_queue,
+                    correlation_id=self.corr_id,
+                ),
+                body=message)
+            self.logger.debug(f"Publishing to OpenFace. Correlation-ID: {self.corr_id}")
+        except KeyboardInterrupt:
+            print("Detected Keyboard Interrupt. Exiting...")
+        return self.response
