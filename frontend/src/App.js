6f84c0e3b1e7c4797e75b2cbe6745c3fadf5e28e
Gulbike Imge Koksal <koksalg15@itu.edu.tr>
2024-06-03 19:38:13 +0200

    add tooltip to toggle buttons in header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -501,15 +501,18 @@ function App() {
                       buttons={[
                         {
                           onClick: () => toggleModal(Tabs.CHAT),
-                          icon: faComment
+                          icon: faComment,
+                          tooltip: "Chat with participants"
                         },
                         {
                           onClick: () => toggleModal(Tabs.INSTRUCTIONS),
-                          icon: faClipboardCheck
+                          icon: faClipboardCheck,
+                          tooltip: "View instructions"
                         },
                         {
                           onClick: () => toggleModal(Tabs.CHATGPT),
-                          externalIcon: true
+                          externalIcon: true,
+                          tooltip: "Chat with GPT"
                         }
                       ]}
                     />
@@ -540,15 +543,18 @@ function App() {
                       buttons={[
                         {
                           onClick: () => toggleModal(Tabs.CHAT),
-                          icon: faComment
+                          icon: faComment,
+                          tooltip: "Chat with participants"
                         },
                         {
                           onClick: () => toggleModal(Tabs.INSTRUCTIONS),
-                          icon: faClipboardCheck
+                          icon: faClipboardCheck,
+                          tooltip: "View instructions"
                         },
                         {
                           onClick: () => toggleModal(Tabs.CHATGPT),
-                          externalIcon: true
+                          externalIcon: true,
+                          tooltip: "Chat with GPT"
                         }
                       ]}
                     />
@@ -627,23 +633,27 @@ function App() {
                     buttons={[
                       {
                         onClick: () => toggleModal(Tabs.CHAT),
-                        icon: faComment
+                        icon: faComment,
+                        tooltip: "Chat with participants"
                       },
-                      {
-                        onClick: () => toggleModal(Tabs.INSTRUCTIONS),
-                        icon: faClipboardCheck
-                      },
+                      // {
+                      //   onClick: () => toggleModal(Tabs.INSTRUCTIONS),
+                      //   icon: faClipboardCheck,
+                      // },
                       {
                         onClick: () => toggleModal(Tabs.PARTICIPANTS),
-                        icon: faUsers
+                        icon: faUsers,
+                        tooltip: "View participants"
                       },
                       {
                         onClick: () => toggleModal(Tabs.FILTER_INFORMATION),
-                        icon: faClipboardList
+                        icon: faClipboardList,
+                        tooltip: "View filter information"
                       },
                       {
                         onClick: () => toggleModal(Tabs.CHATGPT),
-                        externalIcon: true
+                        externalIcon: true,
+                        tooltip: "Chat with GPT"
                       }
                     ]}
                   />
