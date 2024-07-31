5ee86c33b71e39127c4328cf1d17946ea603680d
Gulbike Imge Koksal <koksalg15@itu.edu.tr>
2024-07-31 02:28:19 +0200

    add filter group dropdown

---
+++ frontend/src/components/molecules/FilterGroupDropdown/FilterGroupDropdown.tsx
@@ -0,0 +1,124 @@
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import ListSubheader from "@mui/material/ListSubheader";
+import { ChatFilter, Filter } from "../../../types";
+import MenuItem from "@mui/material/MenuItem";
+import Typography from "@mui/material/Typography";
+import React from "react";
+import { useAppSelector } from "../../../redux/hooks";
+import { selectFiltersDataSession } from "../../../redux/slices/openSessionSlice";
+import chatFiltersData from "../../../chat_filters.json";
+
+interface FilterGroupDropdownProps {
+  selectedFilter: Filter;
+  selectedChatFilter: ChatFilter;
+  handleFilterSelect: (filter: Filter) => void;
+  handleSelectChatFilter: (chatFilter: ChatFilter) => void;
+}
+
+export const FilterGroupDropdown: React.FC<FilterGroupDropdownProps> = ({
+  selectedFilter,
+  selectedChatFilter,
+  handleFilterSelect,
+  handleSelectChatFilter
+}) => {
+  const filtersData = useAppSelector(selectFiltersDataSession);
+  const individualFilters = filtersData.filter((filter) => filter.groupFilter !== true);
+  const groupFilters = filtersData.filter((filter) => filter.groupFilter === true);
+  const chatFilters: ChatFilter[] = chatFiltersData.chat_filters.map((filter: ChatFilter) => {
+    return filter;
+  });
+
+  return (
+    <Box sx={{ display: "flex", justifyContent: "flex-start", gap: 2 }}>
+      <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "flex-start" }}>
+        <FormControl sx={{ m: 1, minWidth: 180 }} size="small">
+          <InputLabel id="filters-select">Filters</InputLabel>
+          {
+            <Select
+              value={selectedFilter.name}
+              id="filters-select"
+              label="Filters"
+              displayEmpty={true}
+              renderValue={(selected) => {
+                if (selected === "Placeholder") {
+                  return <em>Select a Filter</em>;
+                }
+                return selected;
+              }}
+            >
+              <ListSubheader sx={{ fontWeight: "bold", color: "black" }}>
+                Individual Filters
+              </ListSubheader>
+              {/* Uncomment the below block to use new filters. */}
+              {individualFilters.map((individualFilter: Filter) => {
+                return (
+                  <MenuItem
+                    key={individualFilter.id}
+                    value={individualFilter.name}
+                    onClick={() => handleFilterSelect(individualFilter)}
+                  >
+                    {individualFilter.name}
+                  </MenuItem>
+                );
+              })}
+              <ListSubheader sx={{ fontWeight: "bold", color: "black" }}>
+                Group Filters
+              </ListSubheader>
+              {groupFilters.map((groupFilter: Filter) => {
+                return (
+                  <MenuItem
+                    key={groupFilter.id}
+                    value={groupFilter.name}
+                    onClick={() => handleFilterSelect(groupFilter)}
+                  >
+                    {groupFilter.name}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          }
+        </FormControl>
+        <Typography variant="caption">(NOTE: You can select each filter multiple times)</Typography>
+      </Box>
+
+      {chatFilters && (
+        <Box sx={{ display: "flex", justifyContent: "flex-start" }}>
+          <FormControl sx={{ m: 1, minWidth: 180 }} size="small">
+            <InputLabel id="filters-select">Chat Filters (BETA)</InputLabel>
+
+            <Select
+              value={selectedChatFilter.name}
+              id="filters-select"
+              label="Chat Filters (BETA)"
+              displayEmpty={true}
+              renderValue={(selected) => {
+                if (selected === "Placeholder") {
+                  return <em>Select a Chat Filter</em>;
+                }
+                return selected;
+              }}
+            >
+              <ListSubheader sx={{ fontWeight: "bold", color: "black" }}>
+                Individual Filters
+              </ListSubheader>
+              {chatFilters.map((chatFilter: ChatFilter) => {
+                return (
+                  <MenuItem
+                    key={chatFilter.id}
+                    value={chatFilter.name}
+                    onClick={() => handleSelectChatFilter(chatFilter)}
+                  >
+                    {chatFilter.name}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+        </Box>
+      )}
+    </Box>
+  );
+};
