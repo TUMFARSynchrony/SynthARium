57e04b607889bce1f9469ac89f7e5b1b28f5b2b2
muhammed-enes.deniz <menesdeniz99@gmail.com>
2024-09-22 23:47:26 +0200

    Backend - introduce filter mapping to create correct processors

---
+++ backend/post_processing/filter_mapping.py
@@ -0,0 +1,7 @@
+FILTER_CONFIG_MAPPING = {
+    "EDGE_OUTLINE": "manipulative",
+    "ROTATION": "manipulative",
+    "DELAY": "manipulative",
+    "OPENFACE_AU": "analysis",
+    "SYNC_SCORE_GF": "analysis",
+}
